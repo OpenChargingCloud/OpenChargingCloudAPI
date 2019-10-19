6b561a1087270d00ec9c336fbd3f154287261c06
Achim Friedland <github@ahzf.de>
2019-10-19 03:08:06 +0200

    Integrate WWCP_API. Remove WWCP_Net.

---
+++ HTTP_IO/CSV_IO_URITemplates.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Copyright (c) 2014-2018 GraphDefined GmbH <achim.friedland@graphdefined.com>
+ * This file is part of WWCP Net <https://github.com/GraphDefined/WWCP_Net>
+ *
+ * Licensed under the Affero GPL license, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/agpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using org.GraphDefined.Vanaheimr.Illias;
+using org.GraphDefined.Vanaheimr.Hermod;
+using org.GraphDefined.Vanaheimr.Hermod.HTTP;
+using cloud.charging.open.API;
+
+#endregion
+
+namespace org.GraphDefined.WWCP.Net.IO.CSV
+{
+
+    /// <summary>
+    /// WWCP HTTP API - CSV I/O.
+    /// </summary>
+    public static partial class CSV_IO
+    {
+
+        /// <summary>
+        /// Attach CSV I/O to the given WWCP HTTP API.
+        /// </summary>
+        /// <param name="OpenChargingCloudAPI">A WWCP HTTP API.</param>
+        /// <param name="Hostname">Limit this CSV I/O handling to the given HTTP hostname.</param>
+        /// <param name="URIPrefix">A common URI prefix for all URIs within this API.</param>
+        public static void Attach_CSV_IO(this OpenChargingCloudAPI OpenChargingCloudAPI,
+                                         HTTPHostname?      Hostname   = null,
+                                         HTTPPath?          URIPrefix  = null)
+        {
+
+            var _Hostname = Hostname ?? HTTPHostname.Any;
+
+            // /AdminStatus
+            // /Status
+
+            //WWCPAPI.Attach_CSV_IO_RoamingNetworks   (Hostname, URIPrefix);
+            //WWCPAPI.Attach_CSV_IO_ChargingOperators (Hostname, URIPrefix);
+            //WWCPAPI.Attach_CSV_IO_ParkingOperators  (Hostname, URIPrefix);
+            //WWCPAPI.Attach_CSV_IO_EMobilityProviders(Hostname, URIPrefix);
+            //WWCPAPI.Attach_CSV_IO_SmartCities       (Hostname, URIPrefix);
+            //WWCPAPI.Attach_CSV_IO_Reservations      (Hostname, URIPrefix);
+            //WWCPAPI.Attach_CSV_IO_ChargingSessions  (Hostname, URIPrefix);
+
+        }
+
+    }
+
+}
---
+++ OpenChargingCloudAPILogger.cs
@@ -0,0 +1,244 @@
+﻿/*
+ * Copyright (c) 2014-2019 GraphDefined GmbH <achim.friedland@graphdefined.com>
+ * This file is part of WWCP Net <https://github.com/GraphDefined/WWCP_Net>
+ *
+ * Licensed under the Affero GPL license, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/agpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+
+using org.GraphDefined.Vanaheimr.Hermod.HTTP;
+
+#endregion
+
+namespace cloud.charging.open.API
+{
+
+    /// <summary>
+    /// An OpenChargingCloud HTTP API logger.
+    /// </summary>
+    public class OpenChargingCloudAPILogger : HTTPServerLogger
+    {
+
+        #region Data
+
+        /// <summary>
+        /// The default context of this logger.
+        /// </summary>
+        public const String DefaultContext = "OpenChargingCloudAPI";
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The linked OpenChargingCloud API.
+        /// </summary>
+        public OpenChargingCloudAPI  OpenChargingCloudAPI    { get; }
+
+        #endregion
+
+        #region Constructor(s)
+
+        #region OpenChargingCloudAPILogger(OpenChargingCloudAPI, Context = DefaultContext, LogfileCreator = null)
+
+        /// <summary>
+        /// Create a new WWCP HTTP API logger using the default logging delegates.
+        /// </summary>
+        /// <param name="OpenChargingCloudAPI">A WWCP API.</param>
+        /// <param name="Context">A context of this API.</param>
+        /// <param name="LogfileCreator">A delegate to create a log file from the given context and log file name.</param>
+        public OpenChargingCloudAPILogger(OpenChargingCloudAPI    OpenChargingCloudAPI,
+                                          String                  Context         = DefaultContext,
+                                          LogfileCreatorDelegate  LogfileCreator  = null)
+
+            : this(OpenChargingCloudAPI,
+                   Context,
+                   null,
+                   null,
+                   null,
+                   null,
+                   LogfileCreator: LogfileCreator)
+
+        { }
+
+        #endregion
+
+        #region OpenChargingCloudAPILogger(OpenChargingCloudAPI, Context, ... Logging delegates ...)
+
+        /// <summary>
+        /// Create a new WWCP HTTP API logger using the given logging delegates.
+        /// </summary>
+        /// <param name="OpenChargingCloudAPI">A WWCP API.</param>
+        /// <param name="Context">A context of this API.</param>
+        /// 
+        /// <param name="LogHTTPRequest_toConsole">A delegate to log incoming HTTP requests to console.</param>
+        /// <param name="LogHTTPResponse_toConsole">A delegate to log HTTP requests/responses to console.</param>
+        /// <param name="LogHTTPRequest_toDisc">A delegate to log incoming HTTP requests to disc.</param>
+        /// <param name="LogHTTPResponse_toDisc">A delegate to log HTTP requests/responses to disc.</param>
+        /// 
+        /// <param name="LogHTTPRequest_toNetwork">A delegate to log incoming HTTP requests to a network target.</param>
+        /// <param name="LogHTTPResponse_toNetwork">A delegate to log HTTP requests/responses to a network target.</param>
+        /// <param name="LogHTTPRequest_toHTTPSSE">A delegate to log incoming HTTP requests to a HTTP server sent events source.</param>
+        /// <param name="LogHTTPResponse_toHTTPSSE">A delegate to log HTTP requests/responses to a HTTP server sent events source.</param>
+        /// 
+        /// <param name="LogHTTPError_toConsole">A delegate to log HTTP errors to console.</param>
+        /// <param name="LogHTTPError_toDisc">A delegate to log HTTP errors to disc.</param>
+        /// <param name="LogHTTPError_toNetwork">A delegate to log HTTP errors to a network target.</param>
+        /// <param name="LogHTTPError_toHTTPSSE">A delegate to log HTTP errors to a HTTP server sent events source.</param>
+        /// 
+        /// <param name="LogfileCreator">A delegate to create a log file from the given context and log file name.</param>
+        public OpenChargingCloudAPILogger(OpenChargingCloudAPI        OpenChargingCloudAPI,
+                                          String                      Context,
+
+                                          HTTPRequestLoggerDelegate   LogHTTPRequest_toConsole,
+                                          HTTPResponseLoggerDelegate  LogHTTPResponse_toConsole,
+                                          HTTPRequestLoggerDelegate   LogHTTPRequest_toDisc,
+                                          HTTPResponseLoggerDelegate  LogHTTPResponse_toDisc,
+
+                                          HTTPRequestLoggerDelegate   LogHTTPRequest_toNetwork    = null,
+                                          HTTPResponseLoggerDelegate  LogHTTPResponse_toNetwork   = null,
+                                          HTTPRequestLoggerDelegate   LogHTTPRequest_toHTTPSSE    = null,
+                                          HTTPResponseLoggerDelegate  LogHTTPResponse_toHTTPSSE   = null,
+
+                                          HTTPResponseLoggerDelegate  LogHTTPError_toConsole      = null,
+                                          HTTPResponseLoggerDelegate  LogHTTPError_toDisc         = null,
+                                          HTTPResponseLoggerDelegate  LogHTTPError_toNetwork      = null,
+                                          HTTPResponseLoggerDelegate  LogHTTPError_toHTTPSSE      = null,
+
+                                          LogfileCreatorDelegate      LogfileCreator              = null)
+
+            : base(OpenChargingCloudAPI.HTTPServer,//.InternalHTTPServer,
+                   Context,
+
+                   LogHTTPRequest_toConsole,
+                   LogHTTPResponse_toConsole,
+                   LogHTTPRequest_toDisc,
+                   LogHTTPResponse_toDisc,
+
+                   LogHTTPRequest_toNetwork,
+                   LogHTTPResponse_toNetwork,
+                   LogHTTPRequest_toHTTPSSE,
+                   LogHTTPResponse_toHTTPSSE,
+
+                   LogHTTPError_toConsole,
+                   LogHTTPError_toDisc,
+                   LogHTTPError_toNetwork,
+                   LogHTTPError_toHTTPSSE,
+
+                   LogfileCreator)
+
+        {
+
+            #region Initial checks
+
+            if (OpenChargingCloudAPI == null)
+                throw new ArgumentNullException(nameof(OpenChargingCloudAPI), "The given WWCP HTTP API must not be null!");
+
+            #endregion
+
+            this.OpenChargingCloudAPI = OpenChargingCloudAPI;
+
+            #region Register auth start/stop log events
+
+            RegisterEvent2("AuthEVSEStart",
+                          handler => OpenChargingCloudAPI.OnAuthStartEVSERequest += handler,
+                          handler => OpenChargingCloudAPI.OnAuthStartEVSERequest -= handler,
+                          "Auth", "AuthEVSE", "AuthStart", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            RegisterEvent2("AuthEVSEStarted",
+                          handler => OpenChargingCloudAPI.OnAuthStartEVSEResponse += handler,
+                          handler => OpenChargingCloudAPI.OnAuthStartEVSEResponse -= handler,
+                          "Auth", "AuthEVSE", "AuthStarted", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            RegisterEvent2("AuthEVSEStop",
+                          handler => OpenChargingCloudAPI.OnAuthStopEVSERequest += handler,
+                          handler => OpenChargingCloudAPI.OnAuthStopEVSERequest -= handler,
+                          "Auth", "AuthEVSE", "AuthStop", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            RegisterEvent2("AuthEVSEStopped",
+                          handler => OpenChargingCloudAPI.OnAuthStopEVSEResponse += handler,
+                          handler => OpenChargingCloudAPI.OnAuthStopEVSEResponse -= handler,
+                          "Auth", "AuthEVSE", "AuthStopped", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            #endregion
+
+            #region Register remote start/stop log events
+
+            RegisterEvent2("RemoteEVSEStart",
+                          handler => OpenChargingCloudAPI.OnSendRemoteStartEVSERequest += handler,
+                          handler => OpenChargingCloudAPI.OnSendRemoteStartEVSERequest -= handler,
+                          "Remote", "RemoteEVSE", "RemoteStart", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            RegisterEvent2("RemoteEVSEStarted",
+                          handler => OpenChargingCloudAPI.OnSendRemoteStartEVSEResponse += handler,
+                          handler => OpenChargingCloudAPI.OnSendRemoteStartEVSEResponse -= handler,
+                          "Remote", "RemoteEVSE", "RemoteStarted", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            RegisterEvent2("RemoteEVSEStop",
+                          handler => OpenChargingCloudAPI.OnSendRemoteStopEVSERequest += handler,
+                          handler => OpenChargingCloudAPI.OnSendRemoteStopEVSERequest -= handler,
+                          "Remote", "RemoteEVSE", "RemoteStop", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            RegisterEvent2("RemoteEVSEStopped",
+                          handler => OpenChargingCloudAPI.OnSendRemoteStopEVSEResponse += handler,
+                          handler => OpenChargingCloudAPI.OnSendRemoteStopEVSEResponse -= handler,
+                          "Remote", "RemoteEVSE", "RemoteStopped", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            #endregion
+
+            #region Register CDR log events
+
+            RegisterEvent2("SendCDR",
+                          handler => OpenChargingCloudAPI.OnSendCDRsRequest += handler,
+                          handler => OpenChargingCloudAPI.OnSendCDRsRequest -= handler,
+                          "CDR", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            RegisterEvent2("CDRSent",
+                          handler => OpenChargingCloudAPI.OnSendCDRsResponse += handler,
+                          handler => OpenChargingCloudAPI.OnSendCDRsResponse -= handler,
+                          "CDR", "All").
+                RegisterDefaultConsoleLogTarget(this).
+                RegisterDefaultDiscLogTarget(this);
+
+            #endregion
+
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+
+}
