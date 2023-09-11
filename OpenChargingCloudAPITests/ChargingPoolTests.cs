d86686e3de7094415859c4bcb2562954246bec10
Achim Friedland <achim.friedland@graphdefined.com>
2023-09-12 00:54:01 +0200

    .NET7 refactorings

--- OpenChargingCloudAPITests/ChargingPoolTests.cs
+++ OpenChargingCloudAPITests/ChargingPoolTests.cs
@@ -17,15 +17,10 @@
 
 #region Usings
 
-using System;
-using System.Linq;
-using System.Threading;
-
 using NUnit.Framework;
 using Newtonsoft.Json.Linq;
 
 using org.GraphDefined.Vanaheimr.Illias;
-using org.GraphDefined.Vanaheimr.Hermod;
 using org.GraphDefined.Vanaheimr.Hermod.HTTP;
 
 #endregion
@@ -39,7 +34,6 @@ namespace cloud.charging.open.protocols.WWCP.Net.UnitTests
     [TestFixture]
     public class ChargingPoolTests : ATests
     {
-
 
         #region Init()
 
