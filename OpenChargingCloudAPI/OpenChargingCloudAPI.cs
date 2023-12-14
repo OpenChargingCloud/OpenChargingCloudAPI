83e92fe8f5c4e35f448084bc9c1e8b4fa5bcfb7b
Achim Friedland <achim.friedland@graphdefined.com>
2023-12-14 16:23:56 +0100

    Fix

--- OpenChargingCloudAPI/OpenChargingCloudAPI.cs
+++ OpenChargingCloudAPI/OpenChargingCloudAPI.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2014-2023, Achim 'ahzf' Friedland <achim@graphdefined.org>
+ * Copyright (c) 2014-2023, Achim 'ahzf' Friedland <achim@graphdefined.com>
  * This file is part of Open Charging Cloud API <http://www.github.com/OpenChargingCloud/OpenChargingCloudAPI>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
